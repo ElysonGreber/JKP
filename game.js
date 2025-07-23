b9819aa9935365727aa11207aa149f16202f1d50
BlackBloc <49597405+ElysonGreber@users.noreply.github.com>
2025-07-22 23:41:27 -0300

    Add files via upload
    Update Functions

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const CONTRACT_ADDRESS = "0x3Ca407D331D4d1175940f64d329cDB374eA99c90";
+const CONTRACT_ADDRESS = "0x86D78297067E2b9c5f1dF7C3Fa30F9CF1f006ab6";
 const TOKEN_ADDRESS = "0x5DDED6D293111FD9967ED2835Ce905B1eb2c023E";
 
 const abiContrato = [{
@@ -70,6 +70,42 @@ const abiContrato = [{
         ],
         name: "JogoRealizado",
         type: "event",
+    },
+    {
+        inputs: [{
+            internalType: "address",
+            name: "jogador",
+            type: "address"
+        }],
+        name: "verHistorico",
+        outputs: [{
+            components: [{
+                    internalType: "uint8",
+                    name: "jogadorEscolha",
+                    type: "uint8"
+                },
+                {
+                    internalType: "uint8",
+                    name: "contratoEscolha",
+                    type: "uint8"
+                },
+                {
+                    internalType: "uint8",
+                    name: "resultado",
+                    type: "uint8"
+                },
+                {
+                    internalType: "uint256",
+                    name: "timestamp",
+                    type: "uint256"
+                }
+            ],
+            internalType: "struct PedraPapelTesoura.Jogada[]",
+            name: "",
+            type: "tuple[]"
+        }],
+        stateMutability: "view",
+        type: "function"
     },
 ];
 
@@ -161,6 +197,9 @@ async function conectar() {
     token = new web3.eth.Contract(abiERC20, TOKEN_ADDRESS);
 
     await atualizarSaldos();
+    await exibirHistoricoJogadas();
+    mostrarBotaoHistorico();
+
 }
 
 // async function conectar() {
@@ -257,6 +296,7 @@ async function jogar(escolha) {
                     .call();
 
                 mostrarResultado(resultado);
+                await exibirHistoricoJogadas();
 
                 // Se o jogador venceu (2), saca automaticamente
                 if (parseInt(resultado) === 2) {
@@ -270,111 +310,6 @@ async function jogar(escolha) {
         spinner.style.display = "none";
     }
 }
-
-// async function jogar(escolha) {
-//   const spinner = document.getElementById("spinner");
-//   const resultadoEl = document.getElementById("resultado");
-//   const dueloEl = document.getElementById("duelo");
-
-//   try {
-//     spinner.style.display = "block";
-//     dueloEl.style.display = "block";
-//     resultadoEl.innerText = "";
-//     dueloEl.innerText = "";
-
-//     await contrato.methods
-//       .jogar(escolha)
-//       .send({
-//         from: conta,
-//       })
-//       .on("receipt", async (tx) => {
-//         // prettier-ignore
-//         const evento = tx.events?.JogoRealizado?.returnValues;
-//         if (evento) {
-//           const jogadorEscolha = parseInt(evento.jogadorEscolha);
-//           const contratoEscolha = parseInt(evento.contratoEscolha);
-//           const resultado = parseInt(evento.resultado);
-
-//           atualizarDuelo(jogadorEscolha, contratoEscolha);
-//           mostrarResultado(resultado);
-//         } else {
-//           const resultado = await contrato.methods
-//             .verUltimoResultado(conta)
-//             .call();
-//           mostrarResultado(resultado);
-//         }
-//       });
-//   } catch (e) {
-//     console.error(e);
-//     alert("Erro ao jogar.");
-//   } finally {
-//     spinner.style.display = "none";
-//   }
-// }
-// async function jogar(escolha) {
-//     const spinner = document.getElementById("spinner");
-//     const resultadoEl = document.getElementById("resultado");
-//     const dueloEl = document.getElementById("duelo");
-//     const imgJogador = document.getElementById("img-jogador");
-
-//     const imagens = ["pedra.png", "papel.png", "tesoura.png"];
-
-//     try {
-//         // Exibe a escolha do jogador imediatamente
-//         dueloEl.style.display = "block";
-//         imgJogador.src = imagens[escolha];
-//         imgJogador.alt = `Escolha do jogador: ${imagens[escolha].split(".")[0]}`;
-
-//         spinner.style.display = "block";
-//         resultadoEl.innerText = "";
-//         document.getElementById("duelo-texto").innerText = "Aguardando resultado...";
-
-//         await contrato.methods
-//             .jogar(escolha)
-//             .send({ from: conta })
-//             .on("receipt", async(tx) => {
-//                 let evento = null;
-
-//                 if (
-//                     tx.events &&
-//                     tx.events.JogoRealizado &&
-//                     tx.events.JogoRealizado.returnValues
-//                 ) {
-//                     evento = tx.events.JogoRealizado.returnValues;
-//                 }
-
-//                 if (evento) {
-//                     const jogadorEscolha = parseInt(evento.jogadorEscolha);
-//                     const contratoEscolha = parseInt(evento.contratoEscolha);
-//                     const resultado = parseInt(evento.resultado);
-
-//                     atualizarDuelo(jogadorEscolha, contratoEscolha);
-//                     mostrarResultado(resultado);
-//                 } else {
-//                     const dados = await buscarUltimoEventoDaConta(conta);
-//                     if (dados) {
-//                         atualizarDuelo(dados.jogadorEscolha, dados.contratoEscolha);
-//                     }
-//                 }
-
-//                 const resultado = await contrato.methods
-//                     .verUltimoResultado(conta)
-//                     .call();
-
-//                 mostrarResultado(resultado);
-
-//                 // Se o jogador venceu (2), saca automaticamente
-//                 if (parseInt(resultado) === 2) {
-//                     await sacar();
-//                 }
-//             });
-//     } catch (e) {
-//         console.error(e);
-//         alert("Erro ao jogar.");
-//     } finally {
-//         spinner.style.display = "none";
-//     }
-// }
 
 async function buscarUltimoEventoDaConta(conta) {
     try {
@@ -447,167 +382,49 @@ async function atualizarSaldos() {
         console.error("Erro ao buscar saldos:", err);
     }
 }
-// const CONTRACT_ADDRESS = "0x3Ca407D331D4d1175940f64d329cDB374eA99c90";
-// const TOKEN_ADDRESS = "0x5DDED6D293111FD9967ED2835Ce905B1eb2c023E";
+async function exibirHistoricoJogadas() {
+    try {
+        const historico = await contrato.methods.verHistorico(conta).call();
 
-// const abiContrato = [
-//     { "inputs": [], "name": "pagarParaJogar", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-//     {
-//         "inputs": [{ "internalType": "uint8", "name": "escolha", "type": "uint8" }],
-//         "name": "jogar",
-//         "outputs": [],
-//         "stateMutability": "nonpayable",
-//         "type": "function"
-//     },
-//     { "inputs": [], "name": "sacarPremio", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-//     {
-//         "inputs": [{ "internalType": "address", "name": "jogador", "type": "address" }],
-//         "name": "verUltimoResultado",
-//         "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }],
-//         "stateMutability": "view",
-//         "type": "function"
-//     },
-//     {
-//         "anonymous": false,
-//         "inputs": [
-//             { "indexed": true, "internalType": "address", "name": "jogador", "type": "address" },
-//             { "indexed": false, "internalType": "uint8", "name": "jogadorEscolha", "type": "uint8" },
-//             { "indexed": false, "internalType": "uint8", "name": "contratoEscolha", "type": "uint8" },
-//             { "indexed": false, "internalType": "uint8", "name": "resultado", "type": "uint8" }
-//         ],
-//         "name": "JogoRealizado",
-//         "type": "event"
-//     }
-// ];
+        const tabela = document.getElementById("historico");
+        tabela.innerHTML = `
+            <tr>
+                <th>You</th>
+                <th>Contract</th>
+                <th>Result</th>
+                <th>Data</th>
+            </tr>
+        `;
 
-// const abiERC20 = [{
-//         "constant": false,
-//         "inputs": [
-//             { "name": "spender", "type": "address" },
-//             { "name": "value", "type": "uint256" }
-//         ],
-//         "name": "approve",
-//         "outputs": [{ "name": "", "type": "bool" }],
-//         "type": "function"
-//     },
-//     {
-//         "constant": true,
-//         "inputs": [{ "name": "owner", "type": "address" }],
-//         "name": "balanceOf",
-//         "outputs": [{ "name": "", "type": "uint256" }],
-//         "type": "function"
-//     }
-// ];
+        if (historico.length === 0) {
+            tabela.innerHTML += "<tr><td colspan='4'>Nenhuma jogada registrada.</td></tr>";
+            return;
+        }
 
-// let web3, contrato, token, conta;
+        historico.forEach(jogada => {
+            const escolhas = ["Rock", "Paper", "Scissors"];
+            const resultados = ["Loss", "Draw", "Victory"];
+            const data = new Date(jogada.timestamp * 1000).toLocaleString();
 
-// async function conectar() {
-//     if (window.ethereum) {
-//         web3 = new Web3(window.ethereum);
-//         await ethereum.request({ method: 'eth_requestAccounts' });
-//         const contas = await web3.eth.getAccounts();
-//         conta = contas[0];
-//         document.getElementById("conta").innerText = conta;
+            tabela.innerHTML += `
+                <tr>
+                    <td>${escolhas[jogada.jogadorEscolha]}</td>
+                    <td>${escolhas[jogada.contratoEscolha]}</td>
+                    <td>${resultados[jogada.resultado]}</td>
+                    <td>${data}</td>
+                </tr>
+            `;
+        });
+    } catch (err) {
+        console.error("Erro ao buscar histórico:", err);
+    }
+}
 
-//         contrato = new web3.eth.Contract(abiContrato, CONTRACT_ADDRESS);
-//         token = new web3.eth.Contract(abiERC20, TOKEN_ADDRESS);
-//     } else {
-//         alert("Instale o Metamask.");
-//     }
-// }
+function alternarTabela() {
+    const div = document.getElementById("historico-container");
+    div.style.display = div.style.display === "none" ? "block" : "none";
+}
 
-// async function pagar() {
-//     const valor = web3.utils.toWei("1", "ether");
-//     try {
-//         await token.methods.approve(CONTRACT_ADDRESS, valor).send({ from: conta });
-//         await contrato.methods.pagarParaJogar().send({ from: conta });
-//         alert("Pagamento realizado. Agora jogue!");
-//     } catch (e) {
-//         console.error(e);
-//         alert("Erro no pagamento.");
-//     }
-// }
-
-// // async function jogar(escolha) {
-// //     const spinner = document.getElementById("spinner");
-// //     const resultadoEl = document.getElementById("resultado");
-
-// //     try {
-// //         spinner.style.display = "block";
-// //         resultadoEl.innerText = "";
-
-// //         await contrato.methods.jogar(escolha).send({ from: conta })
-// //             .on('receipt', async(tx) => {
-// //                 const eventos = Object.values(tx.events || {});
-// //                 const evento = eventos.length > 0 ? eventos[0].returnValues : null;
-
-// //                 if (evento) {
-// //                     const jogador = parseInt(evento.jogadorEscolha);
-// //                     const contrato = parseInt(evento.contratoEscolha);
-// //                     const resultado = parseInt(evento.resultado);
-
-// //                     mostrarResultado(jogador, contrato, resultado);
-// //                 } else {
-// //                     const resultado = await contrato.methods.verUltimoResultado(conta).call();
-// //                     resultadoEl.innerText = "Resultado: " + resultado;
-// //                 }
-// //             });
-
-// //     } catch (e) {
-// //         console.error(e);
-// //         alert("Erro ao jogar.");
-// //     } finally {
-// //         spinner.style.display = "none";
-// //     }
-// // }
-// async function jogar(escolha) {
-//     const spinner = document.getElementById("spinner");
-//     const resultadoEl = document.getElementById("resultado");
-
-//     try {
-//         spinner.style.display = "block";
-//         resultadoEl.innerText = "";
-
-//         await contrato.methods.jogar(escolha).send({
-//                 from: conta
-//             })
-//             .on('receipt', async(tx) => {
-//                 const evento = tx.events?.JogoRealizado?.returnValues;
-//                 if (evento) {
-//                     const jogadorEscolha = parseInt(evento.jogadorEscolha);
-//                     const contratoEscolha = parseInt(evento.contratoEscolha);
-//                     const resultado = parseInt(evento.resultado);
-//                     mostrarResultado(jogadorEscolha, contratoEscolha, resultado);
-//                 } else {
-//                     const resultado = await contrato.methods.verUltimoResultado(conta).call();
-//                     mostrarResultado(resultado);
-//                 }
-//             });
-
-//     } catch (e) {
-//         console.error(e);
-//         alert("Erro ao jogar.");
-//     } finally {
-//         spinner.style.display = "none";
-//     }
-// }
-// async function sacar() {
-//     try {
-//         await contrato.methods.sacarPremio().send({ from: conta });
-//         alert("Prêmio sacado!");
-//     } catch (e) {
-//         console.error(e);
-//         alert("Erro ao sacar prêmio.");
-//     }
-// }
-
-// function mostrarResultado(jogador, contrato, resultado) {
-//     const r = parseInt(resultado);
-//     const txt = ["❌ Derrota", "🤝 Empate", "🏆 Vitória"];
-//     const j = parseInt(jogador);
-//     const c = parseInt(contrato);
-//     const txt2 = ["Pedra", "Papel", "Tesoura"];
-
-//     document.getElementById("resultado").innerText =
-//         `Jogador:` + txt2[j] + `Contrato:` + txt2[c] + "\n" + "Resultado: " + txt[r];
-// }
+function mostrarBotaoHistorico() {
+    document.getElementById("btn-historico").style.display = "inline-block";
+}
